d458cc03ce8de0fe22ceafb977a0667d288c049f
icepie <icepie.dev@gmail.com>
2020-11-28 12:19:19 +0800

    complementary functions to improve busybox compatibility

--- giwifi-gear-min.sh
+++ giwifi-gear-min.sh
@@ -32,62 +32,6 @@ url_decode() {
 
 }
 
-parse_url() {
-    local query1 query2 path1 path2
-
-    # extract the protocol
-    proto="$(echo $1 | grep :// | sed -e's,^\(.*://\).*,\1,g')"
-
-    if [[ ! -z $proto ]] ; then
-            # remove the protocol
-            url="$(echo ${1/$proto/})"
-
-            # extract the user (if any)
-            login="$(echo $url | grep @ | cut -d@ -f1)"
-
-            # extract the host
-            host="$(echo ${url/$login@/} | cut -d/ -f1)"
-
-            # by request - try to extract the port
-            port="$(echo $host | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')"
-
-            # extract the uri (if any)
-            resource="/$(echo $url | grep / | cut -d/ -f2-)"
-    else
-            url=""
-            login=""
-            host=""
-            port=""
-            resource=$1
-    fi
-
-    # extract the path (if any)
-    path1="$(echo $resource | grep ? | cut -d? -f1 )"
-    path2="$(echo $resource | grep \# | cut -d# -f1 )"
-    path=$path1
-    if [[ -z $path ]] ; then path=$path2 ; fi
-    if [[ -z $path ]] ; then path=$resource ; fi
-
-    # extract the query (if any)
-    query1="$(echo $resource | grep ? | cut -d? -f2-)"
-    query2="$(echo $query1 | grep \# | cut -d\# -f1 )"
-    query=$query2
-    if [[ -z $query ]] ; then query=$query1 ; fi
-
-    # extract the fragment (if any)
-    fragment="$(echo $resource | grep \# | cut -d\# -f2 )"
-
-    # echo "url: $url"
-    # echo "   proto: $proto"
-    # echo "   login: $login"
-    # echo "    host: $host"
-    # echo "    port: $port"
-    # echo "resource: $resource"
-    # echo "    path: $path"
-    # echo "   query: $query"
-    # echo "fragment: $fragment"
-    echo $query
-}
 
 # str
 str_str() {
@@ -99,18 +43,28 @@ str_str() {
     echo -n "$str"
 }
 
+# json
+json_format() {
+    # Del escape character and format text
+    echo $(echo -e $1) | sed "s@\\\\@@g"
+}
+
 # giwifi api
 gw_get_auth_url()
 {
-    GTW=$1
     echo $(curl -s -I -A "$PC_UA" "http://$1:8062/redirect?oriUrl=http://www.baidu.com" | grep "Location" | awk -F ": " '{print $2}')
 }
 
 gw_get_login_page()
 {
-    GTW=$1
-    echo $(curl -s -L -A "$PC_UA" "http://$1:8062/redirect?oriUrl=http://www.baidu.com" | grep "name=")
+    echo $(curl -s -L -A "$PC_UA" "http://$1:8062/redirect?oriUrl=http://www.baidu.com" | grep "name=" | grep "<input")
 }
+
+gw_get_auth_state()
+{
+    echo $(json_format "$(curl -s -A "$PC_UA" "http://$1:$2/wifidog/get_auth_state")")
+}
+
 
 # main
 (
@@ -121,19 +75,21 @@ gw_get_login_page()
     echo GW_ATUH_URL:
     echo '-->' $GW_ATUH_URL
     echo ''
-    QUERY_STRING=$(parse_url $GW_ATUH_URL)
-    echo QUERY_STRING:
-    echo '-->' $QUERY_STRING
-    
+
     echo ''
-    echo $(str_str "$QUERY_STRING" "gw_port=" "&" )
+    GW_PORT=$(str_str "$GW_ATUH_URL" "gw_port=" "&" )
 
     GW_LOGIN_PAGE=$(gw_get_login_page $GW_GTW)
 
+    #echo $GW_LOGIN_PAGE
+
     GW_SIGN=$(url_encode $(str_str "$GW_LOGIN_PAGE" 'name="sign" value="' '"' ))
     GW_PAGE_TIME=$(str_str "$GW_LOGIN_PAGE" 'name="page_time" value="' '"' )
+    GW_AUTH_STATE=$(gw_get_auth_state $GW_GTW $GW_PORT)
 
     echo $GW_SIGN
     echo $GW_PAGE_TIME
+
+	echo $GW_AUTH_STATE
 
 )
