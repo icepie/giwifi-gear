306e35607563aa03a7947013c3e6036e964bd15f
icepie <icepie.dev@gmail.com>
2020-11-29 01:34:05 +0800

    add gw_loginaction api

--- giwifi-gear-min.sh
+++ giwifi-gear-min.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 # config
-GW_GTW=172.21.1.1
-GW_USER=
-GW_PWD=
-
+GW_GTW="172.21.1.2"
+GW_USER="1001"
+GW_PWD="66686"
+GW_BTYPE="pc"
 PC_UA="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
 
 
@@ -32,6 +32,19 @@ url_decode() {
 
 }
 
+urlencode() {
+    # urlencode <string>
+
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:$i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf '%s' "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+
+}
 
 # str
 str_str() {
@@ -54,7 +67,7 @@ get_json_value()
     awk -v json="$1" -v key="$2" -v defaultValue=$3 'BEGIN{
         foundKeyCount = 0
         while (length(json) > 0) {
-            # pos = index(json, "\""key"\""); ## 这行更快一些，但是如果有value是字符串，且刚好与要查找的key相同，会被误认为是key而导致值获取错误
+            # pos = index(json, "\""key"\""); ## This line is faster, but if a value is a string that happens to be the same as the key you are looking for, it will be mistaken for a key and the value will be fetched incorrectly.
             pos = match(json, "\""key"\"[ \\t]*?:[ \\t]*");
             if (pos == 0) {if (foundKeyCount == 0) {print defaultValue;} exit 0;}
 
@@ -109,7 +122,7 @@ gw_get_auth_url()
 
 gw_get_login_page()
 {
-    echo $(curl -s -L -A "$PC_UA" "http://$1:8062/redirect?oriUrl=http://www.baidu.com" | grep "name=" | grep "<input")
+    echo $(curl -s -L -A "$PC_UA" "http://$1:8062/redirect?oriUrl=http://www.baidu.com" | grep "name=")
 }
 
 gw_get_auth_state()
@@ -117,37 +130,111 @@ gw_get_auth_state()
     echo $(json_format "$(curl -s -A "$PC_UA" "http://$1:$2/wifidog/get_auth_state")")
 }
 
+gw_get_auth_state()
+{
+    echo $(json_format "$(curl -s -A "$PC_UA" "http://$1:$2/wifidog/get_auth_state")")
+}
+
+gw_loginaction()
+{
+    # create random three-digit numbers
+    local str=$(date +%S%M)
+    local rannum=${str:1:3}
+
+    echo "$(json_format "$(curl -s  \
+    -A "$PC_UA" \
+    -X POST \
+    -H 'Accept: */*' \
+    -H 'Connection: keep-alive' \
+    -H 'Content-Type: application/x-www-form-urlencoded' \
+    -H 'accept-encoding: gzip, deflate, br' \
+    -H 'accept-language: zh-CN,zh-TW;q=0.8,zh;q=0.6,en;q=0.4,ja;q=0.2' \
+    -H 'cache-control: max-age=0' \
+    -d "$1" \
+    "http://login.gwifi.com.cn/cmps/admin.php/api/loginaction?round=$rannum" | gunzip)")"
+    
+}
 
 # main
 (
+    # gtw auth auth
     GW_GTW_AUTH=$(gw_get_gtw_auth $GW_GTW)
-    echo $GW_GTW_AUTH
-    echo $GW_USER
 
+    echo GW_GTW_AUTH:
+    echo '-->' $GW_GTW_AUTH
+    echo ''
+
+
+    # get auth url
     GW_ATUH_URL=$(gw_get_auth_url $GW_GTW)
     echo GW_ATUH_URL:
     echo '-->' $GW_ATUH_URL
     echo ''
 
-    echo ''
     GW_PORT=$(str_str "$GW_ATUH_URL" "gw_port=" "&" )
 
+
+    # get login page
     GW_LOGIN_PAGE=$(gw_get_login_page $GW_GTW)
+    echo $GW_LOGIN_PAGE
 
-    #echo $GW_LOGIN_PAGE
+    echo GW_LOGIN_PAGE:
+    echo '-->' $GW_LOGIN_PAGE
+    echo ''
+    echo ''
 
+    # get some values from login page
     GW_SIGN=$(url_encode $(str_str "$GW_LOGIN_PAGE" 'name="sign" value="' '"' ))
+    #GW_SIGN=eQUkyCb3F7mqZg%2Bsw7z9CbWY12%2Bs1PHvGcSS8LDB3g7HsqjpW5yxigrTIGot%2FNdbNAWfO8qLw0iigTez6CEqWGi%2B3RWtG%2BSFSp6YFT5aFROQP%2Fun%2FYdnZh738FfMOqFzQwTcgSDryn5df%2FqA68iZRgxJMpaedxAHVe9SaFEGyy52xaqjc4W1ORVKbvqAsLdRKkYbJnfPvEHOWVupJypBg6gUTzwUPUjZPXSbkn7rxxlrX9%2BJ3gRrmAzlFxB6Cifo
     GW_PAGE_TIME=$(str_str "$GW_LOGIN_PAGE" 'name="page_time" value="' '"' )
+
+
+    echo GW_SIGN: $GW_SIGN
+    echo GW_PAGE_TIME: $GW_PAGE_TIME
+    echo ''
+ 
+    # get sauth state json
     GW_AUTH_STATE=$(gw_get_auth_state $GW_GTW $GW_PORT)
-
-    echo $GW_SIGN
-    echo $GW_PAGE_TIME
-
-
     GW_AUTH_STATE_DATA=$(get_json_value $GW_AUTH_STATE 'data')
     GW_ID=$(get_json_value $GW_AUTH_STATE_DATA 'gw_id' | sed 's/"//g' )
-    echo $GW_ID
-    echo $GW_AUTH_STATE
 
+ 
+    
+    echo GW_AUTH_STATE:
+    echo '-->' $GW_AUTH_STATE
+    echo ''
+    echo GW_ID: $GW_ID
+    echo ''
+
+    GW_LOGIN_DATA="""\
+access_type=$(get_json_value $GW_AUTH_STATE_DATA 'access_type' | sed 's/"//g')\
+&acsign=$(get_json_value $GW_AUTH_STATE_DATA 'sign' | sed 's/"//g')\
+&btype=$GW_BTYPE\
+&client_mac=$(url_encode $(get_json_value $GW_AUTH_STATE_DATA 'client_mac' | sed 's/"//g'))\
+&contact_phone=$(get_json_value $GW_AUTH_STATE_DATA 'contact_phone' | sed 's/"//g')\
+&devicemode=""\
+&gw_address=$(str_str "$GW_ATUH_URL" "gw_address=" "&")\
+&gw_id=$(str_str "$GW_ATUH_URL" "gw_id=" "&")\
+&gw_port=$(str_str "$GW_ATUH_URL" "gw_port=" "&")\
+&lastaccessurl=""\
+&logout_reason=$(get_json_value $GW_AUTH_STATE_DATA 'logout_reason' | sed 's/"//g')\
+&mac=$(url_encode $(str_str "$GW_ATUH_URL" "mac=" "&"))\
+&name=$GW_USER\
+&online_time=$(get_json_value $GW_AUTH_STATE_DATA 'online_time' | sed 's/"//g')\
+&page_time=$GW_PAGE_TIME\
+&password=$GW_PWD\
+&sign=$GW_SIGN\
+&station_cloud=$(get_json_value $GW_AUTH_STATE_DATA 'station_cloud' | sed 's/"//g')\
+&station_sn=$(get_json_value $GW_AUTH_STATE_DATA 'station_sn' | sed 's/"//g')\
+&suggest_phone=$(get_json_value $GW_AUTH_STATE_DATA 'suggest_phone' | sed 's/"//g')\
+&url=$(str_str "$GW_ATUH_URL" "url=" "&")\
+&user_agent=""\
+"""
+
+    echo GW_LOGIN_DATA: $GW_LOGIN_DATA
+    echo ''
+
+    GW_LOGIN_RT_DATA=$(gw_loginaction $GW_LOGIN_DATA)
+    echo $GW_LOGIN_RT_DATA
 )
 
