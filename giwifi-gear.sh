3787f26df1503eece2b3dada64adaaec4ba1ce2c
icepie <icepie.dev@gmail.com>
2020-12-13 04:50:52 +0800

    add main func

--- giwifi-gear.sh
+++ giwifi-gear.sh
@@ -1,6 +1,196 @@
 #/bin/sh
-VERSION=0.10
+# giwifi-gear bash cli tool
+# by icepie
 
+#############################################
+## config
+#############################################
+GW_NAME="1010101010101"
+GW_PWD="password"
+GW_PORT_DEF=8060
+RD_URL_PORT_DEF=8062
+PC_UA="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
+PAD_UA="Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"
+
+
+#############################################
+## url handle
+#############################################
+url_encode() {
+    # urlencode <string>
+
+    local length="${#1}"
+    for i in $(seq $length); do
+        local c="${1:$i-1:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf '%s' "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+
+}
+
+url_decode() {
+    # urldecode <string>
+
+    local url_encoded="${1//+/ }"
+    printf '%b' "${url_encoded//%/\\x}"
+
+}
+
+#############################################
+## string handle
+#############################################
+str_str() {
+    #str_str <string> "str" "str"
+
+    local str
+    str="${1#*${2}}"
+    str="${str%%$3*}"
+    echo -n "$str"
+}
+
+#############################################
+## json handle
+#############################################
+get_json_value()
+{
+    awk -v json="$1" -v key="$2" -v defaultValue="$3" 'BEGIN{
+        foundKeyCount = 0
+        while (length(json) > 0) {
+            # pos = index(json, "\""key"\""); ## 这行更快一些，但是如果有value是字符串，且刚好与要查找的key相同，会被误认为是key而导致值获取错误
+            pos = match(json, "\""key"\"[ \\t]*?:[ \\t]*");
+            if (pos == 0) {if (foundKeyCount == 0) {print defaultValue;} exit 0;}
+
+            ++foundKeyCount;
+            start = 0; stop = 0; layer = 0;
+            for (i = pos + length(key) + 1; i <= length(json); ++i) {
+                lastChar = substr(json, i - 1, 1)
+                currChar = substr(json, i, 1)
+
+                if (start <= 0) {
+                    if (lastChar == ":") {
+                        start = currChar == " " ? i + 1: i;
+                        if (currChar == "{" || currChar == "[") {
+                            layer = 1;
+                        }
+                    }
+                } else {
+                    if (currChar == "{" || currChar == "[") {
+                        ++layer;
+                    }
+                    if (currChar == "}" || currChar == "]") {
+                        --layer;
+                    }
+                    if ((currChar == "," || currChar == "}" || currChar == "]") && layer <= 0) {
+                        stop = currChar == "," ? i : i + 1 + layer;
+                        break;
+                    }
+                }
+            }
+
+            if (start <= 0 || stop <= 0 || start > length(json) || stop > length(json) || start >= stop) {
+                if (foundKeyCount == 0) {print defaultValue;} exit 0;
+            } else {
+                print substr(json, start, stop - start);
+            }
+
+            json = substr(json, stop + 1, length(json) - stop)
+        }
+    }'
+}
+
+
+#############################################
+## giwifi api
+#############################################
+
+json_format(){
+	local json=$1
+	# Del escape character and format text
+	echo $(echo -e $1) | sed "s@\\\\@@g"
+}
+
+# giwifi api
+gw_get_gtw_auth()
+{
+	echo $(json_format "$(curl -s -A "$UA" "$1/getApp.htm?action=getAuthState&os=mac")")
+}
+
+gw_logout()
+{
+	echo $(json_format "$(curl -s -A "$UA" "$1/getApp.htm?action=logout")")
+}
+
+gw_get_auth_url()
+{
+    echo $(curl -s -I -A "$UA" "http://$1:8062/redirect?oriUrl=http://www.baidu.com" | grep "Location" | awk -F ": " '{print $2}')
+}
+
+gw_get_login_page()
+{
+    echo $(curl -s -L -A "$UA" "http://$1:8062/redirect?oriUrl=http://www.baidu.com" | grep "name=")
+}
+
+gw_get_hotspot_group()
+{
+    echo $(json_format "$(curl -s -A "$UA" "http:/$1:$2/wifidog/get_hotspot_group")")
+}
+
+gw_get_auth_state()
+{
+    echo $(json_format "$(curl -s -A "$UA" "http://$1:$2/wifidog/get_auth_state")")
+}
+
+gw_loginaction()
+{
+    # create random three-digit numbers
+    local str=$(date +%S%M)
+    local rannum=${str:1:3}
+
+    echo $(json_format "$(curl -s  \
+    -A "$UA" \
+    -X POST \
+    -H 'Accept: */*' \
+    -H 'Connection: keep-alive' \
+    -H 'Content-Type: application/x-www-form-urlencoded' \
+    -H 'accept-encoding: gzip, deflate, br' \
+    -H 'accept-language: zh-CN,zh-TW;q=0.8,zh;q=0.6,en;q=0.4,ja;q=0.2' \
+    -H 'cache-control: max-age=0' \
+    -d "$1" \
+    "http://login.gwifi.com.cn/cmps/admin.php/api/loginaction?round=$rannum" | gunzip)")
+    
+}
+
+gw_rebindmac()
+{
+    # create random three-digit numbers
+    local str=$(date +%S%M)
+    local rannum=${str:1:3}
+
+    echo $(json_format "$(curl -s  \
+    -A "$UA" \
+    -X POST \
+    -H 'Accept: */*' \
+    -H 'Connection: keep-alive' \
+    -H 'Content-Type: application/x-www-form-urlencoded' \
+    -H 'accept-encoding: gzip, deflate, br' \
+    -H 'accept-language: zh-CN,zh-TW;q=0.8,zh;q=0.6,en;q=0.4,ja;q=0.2' \
+    -H 'cache-control: max-age=0' \
+    -d "$1" \
+    "http://login.gwifi.com.cn/cmps/admin.php/api/reBindMac?round=$rannum" | gunzip)")
+    
+}
+
+gw_auth_token()
+{
+    echo $(json_format "$(curl -s -A "$UA" "$1")")
+}
+
+
+#############################################
+## cli related
+#############################################
 ver(){
   echo "\
 giwifi-gear version $VERSION\
