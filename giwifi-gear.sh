c15e406d83b082edbfc6c2850cae42e03bdc285d
icepie <icepie.dev@gmail.com>
2020-12-13 23:15:18 +0800

    update sth

--- giwifi-gear.sh
+++ giwifi-gear.sh
@@ -13,29 +13,28 @@ RD_URL_PORT_DEF=8062
 PC_UA="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
 PAD_UA="Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"
 
-
 #############################################
 ## url handle
 #############################################
 url_encode() {
-    # urlencode <string>
+	# urlencode <string>
 
-    local length="${#1}"
-    for i in $(seq $length); do
-        local c="${1:$i-1:1}"
-        case $c in
-            [a-zA-Z0-9.~_-]) printf '%s' "$c" ;;
-            *) printf '%%%02X' "'$c" ;;
-        esac
-    done
+	local length="${#1}"
+	for i in $(seq $length); do
+		local c="${1:$i-1:1}"
+		case $c in
+		[a-zA-Z0-9.~_-]) printf '%s' "$c" ;;
+		*) printf '%%%02X' "'$c" ;;
+		esac
+	done
 
 }
 
 url_decode() {
-    # urldecode <string>
+	# urldecode <string>
 
-    local url_encoded="${1//+/ }"
-    printf '%b' "${url_encoded//%/\\x}"
+	local url_encoded="${1//+/ }"
+	printf '%b' "${url_encoded//%/\\x}"
 
 }
 
@@ -43,20 +42,19 @@ url_decode() {
 ## string handle
 #############################################
 str_str() {
-    #str_str <string> "str" "str"
+	#str_str <string> "str" "str"
 
-    local str
-    str="${1#*${2}}"
-    str="${str%%$3*}"
-    echo -n "$str"
+	local str
+	str="${1#*${2}}"
+	str="${str%%$3*}"
+	echo -n "$str"
 }
 
 #############################################
 ## json handle
 #############################################
-get_json_value()
-{
-    awk -v json="$1" -v key="$2" -v defaultValue="$3" 'BEGIN{
+get_json_value() {
+	awk -v json="$1" -v key="$2" -v defaultValue="$3" 'BEGIN{
         foundKeyCount = 0
         while (length(json) > 0) {
             # pos = index(json, "\""key"\""); ## 这行更快一些，但是如果有value是字符串，且刚好与要查找的key相同，会被误认为是key而导致值获取错误
@@ -101,7 +99,7 @@ get_json_value()
     }'
 }
 
-json_format(){
+json_format() {
 	local json=$1
 	# Del escape character and format text
 	echo $(echo -e $1) | sed "s@\\\\@@g"
@@ -110,103 +108,98 @@ json_format(){
 #############################################
 ## giwifi api
 #############################################
-gw_get_gtw_auth()
-{
+gw_get_gtw_auth() {
 	echo $(json_format "$(curl -s -A "$UA" "$1/getApp.htm?action=getAuthState&os=mac")")
 }
 
-gw_logout()
-{
+gw_logout() {
 	echo $(json_format "$(curl -s -A "$UA" "$1/getApp.htm?action=logout")")
 }
 
-gw_get_auth_url()
-{
-    echo $(curl -s -I -A "$UA" "http://$1:8062/redirect?oriUrl=http://www.baidu.com" | grep "Location" | awk -F ": " '{print $2}')
+gw_get_auth_url() {
+	echo $(curl -s -I -A "$UA" "http://$1:8062/redirect?oriUrl=http://www.baidu.com" | grep "Location" | awk -F ": " '{print $2}')
 }
 
-gw_get_login_page()
-{
-    echo $(curl -s -L -A "$UA" "http://$1:8062/redirect?oriUrl=http://www.baidu.com" | grep "name=")
+gw_get_login_page() {
+	echo $(curl -s -L -A "$UA" "http://$1:8062/redirect?oriUrl=http://www.baidu.com" | grep "name=")
 }
 
-gw_get_hotspot_group()
-{
-    echo $(json_format "$(curl -s -A "$UA" "http:/$1:$2/wifidog/get_hotspot_group")")
+gw_get_hotspot_group() {
+	echo $(json_format "$(curl -s -A "$UA" "http:/$1:$2/wifidog/get_hotspot_group")")
 }
 
-gw_get_auth_state()
-{
-    echo $(json_format "$(curl -s -A "$UA" "http://$1:$2/wifidog/get_auth_state")")
+gw_get_auth_state() {
+	echo $(json_format "$(curl -s -A "$UA" "http://$1:$2/wifidog/get_auth_state")")
 }
 
-gw_loginaction()
-{
-    # create random three-digit numbers
-    local str=$(date +%S%M)
-    local rannum=${str:1:3}
+gw_loginaction() {
+	# create random three-digit numbers
+	local str=$(date +%S%M)
+	local rannum=${str:1:3}
 
-    echo $(json_format "$(curl -s  \
-    -A "$UA" \
-    -X POST \
-    -H 'Accept: */*' \
-    -H 'Connection: keep-alive' \
-    -H 'Content-Type: application/x-www-form-urlencoded' \
-    -H 'accept-encoding: gzip, deflate, br' \
-    -H 'accept-language: zh-CN,zh-TW;q=0.8,zh;q=0.6,en;q=0.4,ja;q=0.2' \
-    -H 'cache-control: max-age=0' \
-    -d "$1" \
-    "http://login.gwifi.com.cn/cmps/admin.php/api/loginaction?round=$rannum" | gunzip)")
-    
+	echo $(json_format "$(
+		curl -s \
+		-A "$UA" \
+		-X POST \
+		-H 'Accept: */*' \
+		-H 'Connection: keep-alive' \
+		-H 'Content-Type: application/x-www-form-urlencoded' \
+		-H 'accept-encoding: gzip, deflate, br' \
+		-H 'accept-language: zh-CN,zh-TW;q=0.8,zh;q=0.6,en;q=0.4,ja;q=0.2' \
+		-H 'cache-control: max-age=0' \
+		-d "$1" \
+		"http://login.gwifi.com.cn/cmps/admin.php/api/loginaction?round=$rannum" | gunzip
+	)")
+
 }
 
-gw_rebindmac()
-{
-    # create random three-digit numbers
-    local str=$(date +%S%M)
-    local rannum=${str:1:3}
+gw_rebindmac() {
+	# create random three-digit numbers
+	local str=$(date +%S%M)
+	local rannum=${str:1:3}
 
-    echo $(json_format "$(curl -s  \
-    -A "$UA" \
-    -X POST \
-    -H 'Accept: */*' \
-    -H 'Connection: keep-alive' \
-    -H 'Content-Type: application/x-www-form-urlencoded' \
-    -H 'accept-encoding: gzip, deflate, br' \
-    -H 'accept-language: zh-CN,zh-TW;q=0.8,zh;q=0.6,en;q=0.4,ja;q=0.2' \
-    -H 'cache-control: max-age=0' \
-    -d "$1" \
-    "http://login.gwifi.com.cn/cmps/admin.php/api/reBindMac?round=$rannum" | gunzip)")
-    
+	echo $(json_format "$(
+		curl -s \
+		-A "$UA" \
+		-X POST \
+		-H 'Accept: */*' \
+		-H 'Connection: keep-alive' \
+		-H 'Content-Type: application/x-www-form-urlencoded' \
+		-H 'accept-encoding: gzip, deflate, br' \
+		-H 'accept-language: zh-CN,zh-TW;q=0.8,zh;q=0.6,en;q=0.4,ja;q=0.2' \
+		-H 'cache-control: max-age=0' \
+		-d "$1" \
+		"http://login.gwifi.com.cn/cmps/admin.php/api/reBindMac?round=$rannum" | gunzip
+	)")
+
 }
 
-gw_auth_token()
-{
-    echo $(json_format "$(curl -s -A "$UA" "$1")")
+gw_auth_token() {
+	echo $(json_format "$(curl -s -A "$UA" "$1")")
 }
 
 #############################################
 ## cli related
 #############################################
-init(){
-  # check dep
-  if ! [ -x "$(command -v curl)" ]; then
-  echo 'Error: curl is not installed.' >&2
-  exit 1
-  fi
+init() {
+	# check dep
+	if ! [ -x "$(command -v curl)" ]; then
+		echo 'Error: curl is not installed.' >&2
+		exit 1
+	fi
 
-  # set default ua
-  AUTH_UA=$PC_UA
+	# set default ua
+	AUTH_UA=$PC_UA
 }
 
-ver(){
-  echo "\
+ver() {
+	echo "\
 giwifi-gear version $VERSION\
     "
 }
 
-usage(){
-  echo "\
+usage() {
+	echo "\
 giwifi-gear
   A cli tool for login giwifi (multi-platform, fast, small)
 
@@ -236,93 +229,118 @@ optional arguments:
 
 (c) 2020 icepie.dev@gmail.com\
 "
-# 短格式中，选项值为可选的选项，选项值只能紧接选项而不可使用任何符号将其他选项隔开；如-p80，不要写成性-p 80
-# 短格式中，选项值为必有的选项，选项值既可紧接选项也可以使用空格与选项隔开；如-i192.168.1.1，也可写成-i 192.168.1.1
-# 长格式中，选项值为可选的选项，选项值只能使用=号连接选项；如--port=80，不可写成性--port80或--port 80
-# 长格式中，选项值为必有的选项，选项值既可使用=号连接选项也可使用空格连接选项；如--ip=192.168.1.1，也可写成--ip 192.168.1.1
-# 为简便起见，建议凡是短格式都使用“选项+选项值”的形式（-p80），凡是长格式都使用“选项+=+选项值”的形式（--port=80）
+	# 短格式中，选项值为可选的选项，选项值只能紧接选项而不可使用任何符号将其他选项隔开；如-p80，不要写成性-p 80
+	# 短格式中，选项值为必有的选项，选项值既可紧接选项也可以使用空格与选项隔开；如-i192.168.1.1，也可写成-i 192.168.1.1
+	# 长格式中，选项值为可选的选项，选项值只能使用=号连接选项；如--port=80，不可写成性--port80或--port 80
+	# 长格式中，选项值为必有的选项，选项值既可使用=号连接选项也可使用空格连接选项；如--ip=192.168.1.1，也可写成--ip 192.168.1.1
+	# 为简便起见，建议凡是短格式都使用“选项+选项值”的形式（-p80），凡是长格式都使用“选项+=+选项值”的形式（--port=80）
 }
 
-main(){
-# check param
-if [ $# -le 1 ]; then
-  usage
-  exit
-fi
+main() {
+	# # check param
+	# if [ $# -le 0 ]; then
+	#   usage
+	#   exit
+	# fi
 
-# 
-while true
-do
-  case "$1" in
-  -g|--gateway)
-      gateway="$2"
-      echo "gateway:    $gateway" 
-      shift
-      ;;
-  -u|--username)
-      username="$2"
-      echo "username:    $username"
-      shift
-      ;;
-  -p|--password)
-      password="$2"
-      echo "password:    $password"
-      shift
-      ;;
-  -t|--type)
-      type="$2"
-      if [ $type = pc ];then
-        AUTH_UA=$PC_UA
-      elif [ $type = pad ];then
-        AUTH_UA=$PAD_UA
-      elif [ $type = phone ];then
-        echo "Error: phone type is not be supported now!"
-        exit
-      else
-        echo "Error: plz use the true value(pc/pad/phone)!"
-        exit
-      fi
-      echo "type:    $type"
-      echo "UA:      $AUTH_UA"
-      shift
-      ;;
-  -b|--bind)
-      ISBIND=1
-      ;;
-  -q|--quit)
-      ISQUIT=1
-      ;;
-  -d|--daemon)
-      ISDAEMON=1
-      ;;
-  -v|--version)
-      ver
-      exit 1
-      ;;
-  -h|--help)
-      usage
-      exit
-      ;;
-  --)
-    shift
-    break
-    ;;
-  *)
-    echo "$1 is not option" 
-    ;;
-  esac
-  shift
-done
-# 剩余所有未解析到的参数存在$@中，可通过遍历$@来获取
-#for param in "$@"
-#do 
-#  echo "Parameter #$count: $param"
-#done
+	#
+	while true; do
+		case "$1" in
+		-g | --gateway)
+			gateway="$2"
+			shift
+			;;
+		-u | --username)
+			username="$2"
+			echo "username:    $username"
+			shift
+			;;
+		-p | --password)
+			password="$2"
+			echo "password:    $password"
+			shift
+			;;
+		-t | --type)
+			type="$2"
+			if [ $type = pc ]; then
+				AUTH_UA=$PC_UA
+			elif [ $type = pad ]; then
+				AUTH_UA=$PAD_UA
+			elif [ $type = phone ]; then
+				echo "Error: phone type is not be supported now!"
+				exit
+			else
+				echo "Error: plz use the true value(pc/pad/phone)!"
+				exit
+			fi
+			echo "type:    $type"
+			echo "UA:      $AUTH_UA"
+			shift
+			;;
+		-b | --bind)
+			ISBIND=1
+			;;
+		-q | --quit)
+			ISQUIT=1
+			;;
+		-d | --daemon)
+			ISDAEMON=1
+			;;
+		-v | --version)
+			ver
+			exit 1
+			;;
+		-h | --help)
+			usage
+			exit
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			echo "$1 is not option"
+			;;
+		esac
+		shift
+	done
+	# 剩余所有未解析到的参数存在$@中，可通过遍历$@来获取
+	#for param in "$@"
+	#do
+	#  echo "Parameter #$count: $param"
+	#done
 
-if ([ $ISBIND ] && [ $ISQUIT ]) || ([ $ISBIND ] && [ $ISDAEMON ]) || ([ $ISQUIT ] && [ $ISDAEMON ]); then
-  echo "Error: don't use bind, quit and daemon at same time!"
-  exit 1
-fi
+	# check the conflicting parameters
+	if ([ $ISBIND ] && [ $ISQUIT ]) || ([ $ISBIND ] && [ $ISDAEMON ]) || ([ $ISQUIT ] && [ $ISDAEMON ]); then
+		echo "Error: don't use bind, quit and daemon at same time!"
+		exit 1
+	fi
+
+	# check the necessary parameters
+	if [ ! $gateway ]; then
+		echo -n "Plz enter gateway: "
+		read gateway
+	fi
+
+	# quit option
+	if [ $ISQUIT ]; then
+		echo "quiting"
+		exit 1
+	fi
+
+	if [ ! $username ]; then
+		echo -n "Plz enter username: "
+		read username
+	fi
+
+	if [ ! $password ]; then
+		echo -n "Plz enter password: "
+		read password
+	fi
+
+	echo "gateway: $gateway"
+	echo "username: $username"
+	echo "password: $password"
 
 }
 
@@ -335,9 +353,9 @@ fi
 
 #start
 (
-  init
-  eval set -- $(getopt -o g:u:p:t:bdvh --long gateway:,username:,password:,type:,bind,daemon,version,help -- "$@")
-  # 由于是在main函数中才实现参数处理，所以需要使用$@将所有参数传到main函数
+	init
+	eval set -- $(getopt -o g:u:p:t:bqdvh --long gateway:,username:,password:,type:,bind,quit,daemon,version,help -- "$@")
+	# 由于是在main函数中才实现参数处理，所以需要使用$@将所有参数传到main函数
 
-  main $@
+	main $@
 )
