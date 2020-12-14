391c07b9c91dde9588b716cc8865da3727596cd8
icepie <icepie.dev@gmail.com>
2020-12-14 23:00:28 +0800

    version: 0.16

--- giwifi-gear.sh
+++ giwifi-gear.sh
@@ -6,9 +6,9 @@
 ## config
 #############################################
 # tool info
-VERSION=0.11
+VERSION=0.16
 
-# user info 
+# user info
 GW_GTW=""
 GW_USER=""
 GW_PWD=""
@@ -58,6 +58,23 @@ str_str() {
 	echo -n "$str"
 }
 
+logcat()
+{
+    #%Y-%m-%d
+	local time=$(date "+%H:%M:%S")
+
+	# check the log flag
+	if [ "$2" ];then
+		local flag=$2
+	else
+		local flag='I'
+	fi
+
+	if [ "$1" ];then
+		echo "$time" "$flag": "$1"
+	fi
+}
+
 #############################################
 ## json handle
 #############################################
@@ -65,7 +82,7 @@ get_json_value() {
 	awk -v json="$1" -v key="$2" -v defaultValue="$3" 'BEGIN{
         foundKeyCount = 0
         while (length(json) > 0) {
-            # pos = index(json, "\""key"\""); ## 这行更快一些，但是如果有value是字符串，且刚好与要查找的key相同，会被误认为是key而导致值获取错误
+            #pos = index(json, "\""key"\""); ## 这行更快一些，但是如果有value是字符串，且刚好与要查找的key相同，会被误认为是key而导致值获取错误
             pos = match(json, "\""key"\"[ \\t]*?:[ \\t]*");
             if (pos == 0) {if (foundKeyCount == 0) {print defaultValue;} exit 0;}
 
@@ -118,7 +135,7 @@ json_format() {
 #############################################
 gw_get_gtw_auth() {
 	echo $(json_format "$(curl -s -A "$AUTH_UA" "$1/getApp.htm?action=getAuthState&os=mac")")
-    # os type from index.html
+	# os type from index.html
 	# if (isiOS) url += "&os=ios";
 	# if (isAndroid) url += "&os=android";
 	# if (isWinPC) url += "&os=windows";
@@ -164,7 +181,7 @@ gw_loginaction() {
 		-d "$1" \
 		"http://login.gwifi.com.cn/cmps/admin.php/api/loginaction?round=$rannum" | gunzip
 	)")
-    # use gunzip cause some curl not support --compressed option
+	# use gunzip cause some curl not support --compressed option
 }
 
 gw_rebindmac() {
@@ -185,7 +202,7 @@ gw_rebindmac() {
 		-d "$1" \
 		"http://login.gwifi.com.cn/cmps/admin.php/api/reBindMac?round=$rannum" | gunzip
 	)")
-    # use gunzip cause some curl not support --compressed option
+	# use gunzip cause some curl not support --compressed option
 }
 
 gw_auth_token() {
@@ -204,7 +221,7 @@ init() {
 
 	# set default ua
 	AUTH_UA=$PC_UA
-    GW_BTYPE="pc"
+	GW_BTYPE="pc"
 }
 
 ver() {
@@ -273,11 +290,11 @@ main() {
 			type="$2"
 			# if [ $type = pc ]; then
 			# 	AUTH_UA=$PC_UA
-            #   GW_BTYPE="pc"
+			#   GW_BTYPE="pc"
 			# elif ...
-            if [ $type = pad ]; then
+			if [ $type = pad ]; then
 				AUTH_UA=$PAD_UA
-                GW_BTYPE="pad"
+				GW_BTYPE="pad"
 			elif [ $type = phone ]; then
 				echo "Error: phone type is not be supported now!"
 				exit
@@ -332,123 +349,207 @@ main() {
 		read GW_GTW
 	fi
 
-    # gtw auth auth
-    GW_GTW_AUTH=$(gw_get_gtw_auth $GW_GTW)
-    [ $GW_GTW_AUTH ] || echo "Failed to get the gateway info, plz check the gateway host" && exit;
+	# gtw auth auth
+    logcat "Try to get the auth info..."
+	GW_GTW_AUTH_RTE=$(gw_get_gtw_auth $GW_GTW)
+	[ ! "$GW_GTW_AUTH_RTE" ] && \
+    logcat "Failed to get the gateway info, plz check the gateway host" "E" && \
+    exit 1;
 
-	if [ $ISINFO ]; then
-        echo "GW_GTW_AUTH: "
-        echo '-->' $GW_GTW_AUTH
-        echo ''
-	fi
+	[ $ISINFO ] && \
+    echo "" && \
+    echo "GW_GTW_AUTH_RTE: " && \
+    echo "--> $GW_GTW_AUTH_RTE" && \
+    echo "";
 
-	# quit option
+
+    GW_GTW_AUTH_RTE_DATA="$(get_json_value $GW_GTW_AUTH_RTE 'data')"
+
+    if [ "$(get_json_value $GW_GTW_AUTH_RTE_DATA 'auth_state')" -eq 2 ]; then
+        # 2 is logged
+        logcat "Good! you are authed!"
+
+        [ ! $ISQUIT ] && \
+        logcat "exit" && \
+        exit 1;
+    else
+        logcat "The device not authed!"
+    fi
+
+        # quit option
 	if [ $ISQUIT ]; then
+        logcat "Try to login out "
+
 		GW_QUIT_RTE=$(gw_logout $GW_GTW)
-        echo $GW_QUIT_RTE
+        [ ! "$GW_GTW_AUTH_RTE" ] && \
+        logcat "Failed to get the quit info, plz retry" "E" && \
+        exit 1;
+
+        [ $ISINFO ] && \
+        echo "" && \
+        echo "GW_QUIT_RTE: " && \
+        echo "--> $GW_QUIT_RTE" && \
+        echo "";
+
+        if [ $(get_json_value $GW_QUIT_RTE 'resultCode') -eq 0 ]; then
+            logcat "sign out of account successfully"
+        else
+            logcat "Failed to sign out of account"
+        fi
 		exit 1
 	fi
 
-	if [ ! $GW_USER ]; then
-		echo -n "Plz enter username: "
-		read GW_USER
-	fi
 
-	if [ ! $GW_PWD ]; then
-		echo -n "Plz enter password: "
-		read GW_PWD
-	fi
+    # check the username and password
+	[ ! "$GW_USER" ] && \
+    echo -n "Plz enter username: " && \
+	read GW_USER
 
-	echo "gateway: $GW_GTW"
-	echo "username: $GW_USER"
-	echo "password: $GW_PWD"
+	[ ! "$GW_PWD" ] && \
+    echo -n "Plz enter password: " && \
+    read GW_PWD
 
-    # get login page
-    GW_LOGIN_PAGE=$(gw_get_login_page $GW_GTW)
-    [ $GW_LOGIN_PAGE ] || echo "Failed to get the login page, plz try again" && exit 1;
+	# get login page
+	GW_LOGIN_PAGE=$(gw_get_login_page $GW_GTW)
+	[ ! "$GW_LOGIN_PAGE" ] && \
+    logcat "Failed to get the login page, plz try again" "E" && \
+    exit 1;
 
-    if [ $ISINFO ]; then
-        echo $GW_LOGIN_PAGE
+	[ $ISINFO ] && \
+    echo "GW_LOGIN_PAGE: " && \
+	echo "--> $GW_LOGIN_PAGE" && \
+	echo ""
 
-        echo GW_LOGIN_PAGE:
-        echo '-->' $GW_LOGIN_PAGE
-        echo ''
-        echo ''
-    fi
+	# get auth url
+	GW_AUTH_URL=$(gw_get_auth_url $GW_GTW)
+    [ ! "$GW_AUTH_URL" ] && \
+    logcat "Failed to get the auth url, plz try again" "E" && \
+    exit 1;
 
-    # get auth url
-    GW_AUTH_URL=$(gw_get_auth_url $GW_GTW)
-    if [ $ISINFO ]; then
-        echo GW_AUTH_URL:
-        echo '-->' $GW_AUTH_URL
-        echo ''
-    fi
+	[ $ISINFO ] && \
+	echo "GW_AUTH_URL: " && \
+	echo "--> $GW_AUTH_URL" && \
+	echo "" && \
 
-    # get the giwfi login page port
+	# get the giwfi login page port
+	GW_PORT=$(str_str "$GW_AUTH_URL" "gw_port=" "&")
+
+	# get some values from login page
+	GW_SIGN=$(str_str "$GW_LOGIN_PAGE" 'name="sign" value="' '"')
+	GW_PAGE_TIME=$(str_str "$GW_LOGIN_PAGE" 'name="page_time" value="' '"')
+
+	[ $ISINFO ] && \
+	echo "GW_SIGN: $GW_SIGN" && \
+	echo "GW_PAGE_TIME: $GW_PAGE_TIME" && \
+	echo ""
+
+	# get sauth state json
+	GW_AUTH_STATE_RTE=$(gw_get_auth_state $GW_GTW $GW_PORT)
+	[ ! "$GW_AUTH_STATE_RTE" ] && \
+    logcat "Failed to get the auth state, plz try again" "E" && \
+    exit 1;
+
+	GW_AUTH_STATE_RTE_DATA=$(get_json_value $GW_AUTH_STATE_RTE 'data')
+	GW_ID=$(get_json_value "$GW_AUTH_STATE_RTE_DATA" 'gw_id')
+
+	[ $ISINFO ] && \
+	echo "GW_AUTH_STATE_RTE:" && \
+	echo "--> $GW_AUTH_STATE_RTE" && \
+	echo "" && \
+	echo "GW_ID: $GW_ID"
+
+    GW_APMAC=$(str_str "$GW_AUTH_URL" "apmac=" "&")
+    GW_ADDRESS=$(str_str "$GW_AUTH_URL" "gw_address=" "&")
     GW_PORT=$(str_str "$GW_AUTH_URL" "gw_port=" "&")
+    DEV_IP=$(str_str "$GW_AUTH_URL" "ip=" "&")
+    DEV_MAC=$(str_str "$GW_AUTH_URL" "mac=" "&")
+    STATION_SN=$(get_json_value $GW_AUTH_STATE_RTE_DATA 'station_sn')
 
-    # get some values from login page
-    GW_SIGN=$(str_str "$GW_LOGIN_PAGE" 'name="sign" value="' '"')
-    GW_PAGE_TIME=$(str_str "$GW_LOGIN_PAGE" 'name="page_time" value="' '"')
-    if [ $ISINFO ]; then
-        echo GW_SIGN: $GW_SIGN
-        echo GW_PAGE_TIME: $GW_PAGE_TIME
-        echo ''
-    fi
-
-    # get sauth state json
-    GW_AUTH_STATE=$(gw_get_auth_state $GW_GTW $GW_PORT)
-    GW_AUTH_STATE_DATA=$(get_json_value $GW_AUTH_STATE 'data')
-    GW_ID=$(get_json_value $GW_AUTH_STATE_DATA 'gw_id')
-    if [ $ISINFO ]; then
-        echo GW_AUTH_STATE:
-        echo '-->' $GW_AUTH_STATE
-        echo ''
-        echo GW_ID: $GW_ID
-        echo ''
-    fi
-
-    # login to get the auth token
-    GW_LOGIN_DATA=$(echo """\
-access_type=$(get_json_value $GW_AUTH_STATE_DATA 'access_type')\
-&acsign=$(get_json_value $GW_AUTH_STATE_DATA 'sign')\
+	# login to get the auth token
+	GW_LOGIN_DATA=$(echo """\
+access_type=$(get_json_value $GW_AUTH_STATE_RTE_DATA 'access_type')\
+&acsign=$(get_json_value $GW_AUTH_STATE_RTE_DATA 'sign')\
 &btype=$GW_BTYPE\
-&client_mac=$(url_encode $(get_json_value $GW_AUTH_STATE_DATA 'client_mac'))\
-&contact_phone=$(get_json_value $GW_AUTH_STATE_DATA 'contact_phone')\
+&client_mac=$(url_encode $(get_json_value $GW_AUTH_STATE_RTE_DATA 'client_mac'))\
+&contact_phone=$(get_json_value $GW_AUTH_STATE_RTE_DATA 'contact_phone')\
 &devicemode=""\
-&gw_address=$(str_str "$GW_AUTH_URL" "gw_address=" "&")\
-&gw_id=$(str_str "$GW_AUTH_URL" "gw_id=" "&")\
-&gw_port=$(str_str "$GW_AUTH_URL" "gw_port=" "&")\
+&gw_address=$GW_ADDRESS\
+&gw_id=$GW_ID\
+&gw_port=$GW_PORT\
 &lastaccessurl=""\
-&logout_reason=$(get_json_value $GW_AUTH_STATE_DATA 'logout_reason')\
-&mac=$(url_encode $(str_str "$GW_AUTH_URL" "mac=" "&"))\
+&logout_reason=$(get_json_value $GW_AUTH_STATE_RTE_DATA 'logout_reason')\
+&mac=$(url_encode $DEV_MAC)\
 &name=$GW_USER\
-&online_time=$(get_json_value $GW_AUTH_STATE_DATA 'online_time')\
+&online_time=$(get_json_value $GW_AUTH_STATE_RTE_DATA 'online_time')\
 &page_time=$GW_PAGE_TIME\
 &password=$GW_PWD\
 &sign=$(url_encode $GW_SIGN)\
-&station_cloud=$(get_json_value $GW_AUTH_STATE_DATA 'station_cloud')\
-&station_sn=$(get_json_value $GW_AUTH_STATE_DATA 'station_sn')\
-&suggest_phone=$(get_json_value $GW_AUTH_STATE_DATA 'suggest_phone')\
+&station_cloud=$(get_json_value $GW_AUTH_STATE_RTE_DATA 'station_cloud')\
+&station_sn=$STATION_SN\
+&suggest_phone=$(get_json_value $GW_AUTH_STATE_RTE_DATA 'suggest_phone')\
 &url=$(str_str "$GW_AUTH_URL" "url=" "&")\
 &user_agent=""\
-"""  | sed 's/"//g' )
+""" | sed 's/"//g')
 
-    if [ $ISINFO ]; then
-        echo GW_LOGIN_DATA: $GW_LOGIN_DATA
-        echo ''
-    fi
+	[ $ISINFO ] && \
+    echo "GW_LOGIN_DATA: $GW_LOGIN_DATA" && \
+	echo ''
 
-    if [ $ISBIND ]; then
-        GW_BIND_DATA=$GW_LOGIN_DATA+"&is_signed=2"
-        echo $GW_BIND_DATA
-        GW_BIND_RTE=$(gw_rebindmac $GW_BIND_DATA)
-        echo $GW_BIND_RTE
-    else
-        GW_LOGIN_RTE=$(gw_loginaction $GW_LOGIN_DATA)
-        echo $GW_LOGIN_RTE
-    fi
-    
+	if [ $ISBIND ]; then
+        # bind post need add some data
+		GW_BIND_DATA=$GW_LOGIN_DATA+"&is_signed=2"
+		echo $GW_BIND_DATA
+		GW_BIND_RTE=$(gw_rebindmac $GW_BIND_DATA)
+		echo $GW_BIND_RTE
+	else
+        logcat "Try to get the token whit $GW_BTYPE type..."
+
+		GW_LOGIN_RTE=$(gw_loginaction $GW_LOGIN_DATA)
+        [ ! "$GW_AUTH_URL" ] && \
+        logcat "Failed to get the loginaction, plz try again" "E" && \
+        exit 1;
+
+        [ $ISINFO ] && \
+        echo "GW_LOGIN_RTE:" && \
+        echo "--> $GW_LOGIN_RTE" && \
+        echo ""
+
+        GW_LOGIN_RTE_STATUS=$(get_json_value "$GW_LOGIN_RTE" 'status')
+        GW_LOGIN_RTE_INFO=$(get_json_value "$GW_LOGIN_RTE" 'info' | sed 's/"//g')
+        
+        # check the login status
+        if [ "$GW_LOGIN_RTE_STATUS" -eq 1 ]; then
+            logcat "Get the auth token successfully"
+            logcat "Try to auth whth token..."
+            # the last step
+            GW_AUTH_RTE=$(gw_auth_token $GW_LOGIN_RTE_INFO)
+
+            [ ! "$GW_AUTH_RTE" ] && \
+            logcat "Failed to auth use the token, plz try again" "E" && \
+            exit 1;
+
+            # clear screen
+            cls || clear
+
+            echo """--------------------------------------------
+SSID:             $GW_ID
+AP MAC:           $GW_APMAC
+GateWay:          $GW_ADDRESS
+IP:               $DEV_IP
+MAC:              $DEV_MAC
+Station SN:       $STATION_SN
+Logged:           yes
+--------------------------------------------"""
+            logcat "Auth successfully"
+
+        else
+            logcat "Failed to get the auth token" "E"
+            logcat "Info: $GW_LOGIN_RTE_INFO"
+            exit 1
+        fi
+
+	fi
+
 }
 
 #start
