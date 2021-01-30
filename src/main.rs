ccc7325a231b7403a859892355a3a28e1e8032d6
icepie <icepie.dev@gmail.com>
2021-01-31 01:28:44 +0800

    use hyper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::Client;
+use hyper::{Body, Method, Request};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let req = Request::builder()
+        .method(Method::POST)
+        .uri("http://baidu.com")
+        .body(Body::from("echo"))?;
+
+
+    let client = Client::new();
+    let resp = client.request(req).await?;
+    println!("Response: {}", resp.status());
+    println!("{:?}", hyper::body::to_bytes(resp.into_body()).await.unwrap());
+
+    Ok(())
 }
