e790d1704557784df5b20a7723c0f8933c8804ae
Tea <icepie.dev@gmail.com>
2021-04-12 01:35:29 +0800

    fix echo in sh

--- giwifi-gear-app-pc.sh
+++ giwifi-gear-app-pc.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 # giwifi-gear-pc bash cli tool 
 # by icepie
 
 function get_challge () {
-    local make_pass=$(echo -n $1 | base64)
+    local make_pass=$(printf $1 | base64)
     local token_challge=$2
 
     local i
@@ -14,11 +14,12 @@ function get_challge () {
         res+=${make_pass:$i-1:1}
         res+=${token_challge:32-$i:1}
     done
-    
-    echo -n $res
+
+    printf $res
 }
 
 test=$(get_challge yiyi6666 35f6aa491f6c695c0d77cdce56b94882)
-if [[ "$test" = "e2W8l854a9TbY625NejcYd=c" ]] ; then
+echo $test
+if [ "$test" = "e2W8l854a9TbY625NejcYd=c" ] ; then
     echo 'nice!'
 fi
