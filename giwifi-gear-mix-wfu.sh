513fe0676d250e59a47d50509657e24f9f1d056b
Tea <icepie.dev@gmail.com>
2021-04-21 13:25:34 +0800

    back to giwifi-mix-crypto

--- giwifi-gear-mix-wfu.sh
+++ giwifi-gear-mix-wfu.sh
@@ -6,13 +6,16 @@
 ## config
 #############################################
 # tool info
-VERSION=0.1
+VERSION=0.2
 
 GW_USER=""
 GW_PWD=""
 GW_URL="210.44.64.60"
 
-#CRYPTO_TOOL="./giwifi-mix-crypto"
+# you need to config the crypto tool path
+# go: https://github.com/icepie/giwifi-gear/releases/tag/crypto-go-beta1
+# rs: https://github.com/icepie/giwifi-gear/releases/tag/crypto-rs-beta1
+CRYPTO_TOOL="./giwifi-mix-crypto"
 
 # auth setting
 PC_UA="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
@@ -144,68 +147,72 @@ crypto_encode() {
     
     # aes-128-cbc PKCS5Padding (Original data is ZeroPadding, but it is universal on decryption)
     # default key: 1234567887654321
-    printf '%s' "$1" | openssl enc -e -aes-128-cbc -K $(str2hex "$2") -iv $(str2hex "$3") -nosalt -base64 -A
+    printf '%s' "$1" | openssl enc -e -aes-128-cbc -K "$(str2hex "$2")" -iv "$(str2hex "$3")" -nosalt | base64
 
 }
 
+gw_mix_crypto() {
+    printf '%s' "$($CRYPTO_TOOL -t "$1" -k "$2" -i "$3")"
+}
+
 gw_get_login_page() {
-    echo $(curl -s -L -A "$PC_UA" "http://$1/gportal/web/login" | grep "name=")
+    printf '%s' "$(curl -s -L -A "$PC_UA" "http://$1/gportal/web/login" | grep 'name=')"
 }
 
 gw_loginaction() {
 	# create random three-digit numbers
-	local str=$(date +%S%M)
-	local rannum=${str:1:3}
+	local str="$(date +%S%M)"
+	local rannum="${str:1:3}"
 
-	echo $(curl -s \
+	printf '%s' "$(curl -s \
 		-A "$PC_UA" \
 		-X POST \
 		-d "$1" \
 		"http://$GW_URL/gportal/web/authLogin?round=$rannum"
-	)
+	)"
 }
 
 
 # get the login page
-GW_LOGIN_PAGE=$(gw_get_login_page $GW_URL)
+GW_LOGIN_PAGE="$(gw_get_login_page $GW_URL)"
 
 # get params form the login page
-GW_NAS_NAME=$(str_str "$GW_LOGIN_PAGE" 'name="nasName" value="' '"')
-GW_NAS_IP=$(str_str "$GW_LOGIN_PAGE" 'name="nasIp" value="' '"')
-GW_USER_IP=$(str_str "$GW_LOGIN_PAGE" 'name="userIp" value="' '"')
-GW_USER_MAC=$(str_str "$GW_LOGIN_PAGE" 'id="userMac" value="' '"')
-GW_SSID=$(str_str "$GW_LOGIN_PAGE" 'name="ssid" value="' '"')
-GW_AP_MAC=$(str_str "$GW_LOGIN_PAGE" 'name="apMac" value="' '"')
-GW_PID=$(str_str "$GW_LOGIN_PAGE" 'name="pid" value="' '"')
-GW_VLAN=$(str_str "$GW_LOGIN_PAGE" 'name="vlan" value="' '"')
-GW_SIGN=$(str_str "$GW_LOGIN_PAGE" 'name="sign" value="' '"')
-GW_IV=$(str_str "$GW_LOGIN_PAGE" 'id="iv" value="' '"')
+GW_NAS_NAME="$(str_str "$GW_LOGIN_PAGE" 'name="nasName" value="' '"')"
+GW_NAS_IP="$(str_str "$GW_LOGIN_PAGE" 'name="nasIp" value="' '"')"
+GW_USER_IP="$(str_str "$GW_LOGIN_PAGE" 'name="userIp" value="' '"')"
+GW_USER_MAC="$(str_str "$GW_LOGIN_PAGE" 'id="userMac" value="' '"')"
+GW_SSID="$(str_str "$GW_LOGIN_PAGE" 'name="ssid" value="' '"')"
+GW_AP_MAC="$(str_str "$GW_LOGIN_PAGE" 'name="apMac" value="' '"')"
+GW_PID="$(str_str "$GW_LOGIN_PAGE" 'name="pid" value="' '"')"
+GW_VLAN="$(str_str "$GW_LOGIN_PAGE" 'name="vlan" value="' '"')"
+GW_SIGN="$(str_str "$GW_LOGIN_PAGE" 'name="sign" value="' '"')"
+GW_IV="$(str_str "$GW_LOGIN_PAGE" 'id="iv" value="' '"')"
 
 # nasName=WFXY_NE40E-X8_GateWay_01&nasIp=&userIp=100.65.5.150&userMac=&ssid=&apMac=&pid=20&vlan=&sign=x55KxWFCzAjljfemHhSdI%252B%252FKDtIOOwt6gmiARCrIC%252BA6ithbMIml7022fXxzhl24X3T1CsfsZ%252Bajyc%252BiNT%252FM6A%253D%253D&iv=f86ce181a5dd74b5&name=&password=
 GW_LOGIN_RAW_DATA="""\
-nasName=$GW_NAS_NAME\
-&nasIp=$GW_NAS_IP\
-&userIp=$GW_USER_IP\
-&userMac=$(url_encode $GW_USER_MAC)\
-&ssid=$GW_SSID\
-&apMac=$(url_encode $GW_AP_MAC)\
-&pid=$GW_PID\
-&vlan=$GW_VLAN\
-&sign=$(url_encode $GW_SIGN)\
-&iv=$GW_IV\
-&name=$GW_USER\
-&password=$GW_PWD\
+nasName="$GW_NAS_NAME"\
+&nasIp="$GW_NAS_IP"\
+&userIp="$GW_USER_IP"\
+&userMac="$(url_encode "$GW_USER_MAC")"\
+&ssid="$GW_SSID"\
+&apMac="$(url_encode "$GW_AP_MAC")"\
+&pid="$GW_PID"\
+&vlan="$GW_VLAN"\
+&sign="$(url_encode "$GW_SIGN")"\
+&iv="$GW_IV"\
+&name="$GW_USER"\
+&password="$GW_PWD"\
 """
 
-# echo "$GW_LOGIN_RAW_DATA \
-#  "
+echo "$GW_LOGIN_RAW_DATA \
+"
 
 # use giwifi-mix-crypto to build the login data
-GW_LOGIN_DATA="data="$(url_encode "$(crypto_encode "$GW_LOGIN_RAW_DATA" '1234567887654321' "$GW_IV")")"&iv=$GW_IV"
+GW_LOGIN_DATA="data="$(url_encode "$(gw_mix_crypto "$GW_LOGIN_RAW_DATA" '1234567887654321' "$GW_IV")")"&iv="$GW_IV""
 
 echo $GW_LOGIN_DATA
 
-GW_LOGIN_RTE=$(gw_loginaction $GW_LOGIN_DATA)
+GW_LOGIN_RTE="$(gw_loginaction "$GW_LOGIN_DATA")"
 ## {"data":{"resultCode":2,"reasoncode":1},"info":"u7ec8u7aefu5df2u8ba4u8bc1","status":0}
 
 echo $GW_LOGIN_RTE
